0136499895bc58ab9510d19e96c6094e0343053b
Doctor Andonuts <gowingjm@gmail.com>
2015-08-05 07:14:06 -0400

    ListView

---
+++ app/src/main/java/com/doctorandonuts/taskwarrior/Task.java
@@ -0,0 +1,31 @@
+package com.doctorandonuts.taskwarrior;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Mr Saturn on 8/4/2015.
+ */
+public class Task {
+
+    private String description;
+
+    public void importTask(JSONObject jsonTask) {
+
+    }
+
+//    "id":19,
+//    "brainpower":"M",
+//    "description":"Pay Medical bill",
+//    "entry":"20150529T202744Z",
+//    "estimate":"15min",
+//    "modified":"20150803T230955Z",
+//    "priority":"H",
+//    "status":"pending",
+//    "tags":["home","computer"],
+//    "uuid":"b0c9e6e1-4345-4203-91f5-e87b686218c2",
+//    "urgency":7.26712
+
+}
--- app/src/main/java/com/doctorandonuts/taskwarrior/TaskWarriorActivityFragment.java
+++ app/src/main/java/com/doctorandonuts/taskwarrior/TaskWarriorActivityFragment.java
@@ -7,24 +7,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.doctorandonuts.taskwarrior.sync.Msg;
-import com.doctorandonuts.taskwarrior.sync.TLSClient;
 import com.doctorandonuts.taskwarrior.sync.TaskWarriorSync;
 
-import java.io.IOException;
-import java.nio.charset.MalformedInputException;
-import java.security.cert.CertificateException;
-import java.text.ParseException;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class TaskWarriorActivityFragment extends Fragment {
     TextView textOutput;
-
-
+    ListView listView;
 
     public TaskWarriorActivityFragment() {}
 
@@ -34,6 +30,7 @@ public class TaskWarriorActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_task_warrior, container, false);
 
         textOutput = (TextView)rootView.findViewById(R.id.textOutput);
+        listView = (ListView)rootView.findViewById(R.id.listView);
         return rootView;
     }
 
@@ -47,21 +44,38 @@ public class TaskWarriorActivityFragment extends Fragment {
     private class updateTaskWarriorData extends AsyncTask<Void, Void, String> {
         protected String doInBackground(Void... params) {
             TaskWarriorSync taskWarriorSync = new TaskWarriorSync();
-            String data = taskWarriorSync.getTaskWarriorData();
-            return data;
+            //String data = taskWarriorSync.getTaskWarriorData();
+            //return data;
+            return "DATA";
         }
         protected void onPostExecute(String data) {
             renderTaskWarrior(data);
         }
     }
 
-
-
     private void renderTaskWarrior(String data) {
         try {
-            textOutput.setText(data);
+            textOutput.setText("START");
+
+//            Task[] task = new Task[4];
+//            for(Integer i=0; i<4; i++) {
+//                task[i] = new Task();
+//                task[i].addText("Task: " + i.toString());
+//            }
+
+            final ArrayList<String> list = new ArrayList<>();
+            for (Integer i = 0; i < 4; i++) {
+                list.add(i.toString());
+            }
+            final ArrayAdapter adapter = new ArrayAdapter(getActivity(),
+                    android.R.layout.simple_list_item_1, list);
+            listView.setAdapter(adapter);
+
+            textOutput.setText("END");
+
         }catch(Exception e){
-            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+            Log.e("TaskWarrior", "PROBLEM:" + e.toString());
         }
     }
 }
+
